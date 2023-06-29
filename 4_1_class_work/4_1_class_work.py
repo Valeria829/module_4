b37e1444588d3cb6102d9d875f7071567af40a0c
Valeria829 <136175996+Valeria829@users.noreply.github.com>
2023-06-29 11:06:33 +0500

    Update 4_1_class_work.py

--- 4_1_class_work/4_1_class_work.py
+++ 4_1_class_work/4_1_class_work.py
@@ -1,16 +1,7 @@
-s = input()
-
-if s == s[::-1]:
-    print(True)
-else:
-    print(False)
-
-
 def str_pol():
-    s = input()
+    s = input('Введите полиндром: ')
+
     # print(True if s==s[::-1] else False)
     print(s == s[::-1])
 
 str_pol()
-
-#
