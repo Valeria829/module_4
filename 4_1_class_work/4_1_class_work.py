c9b3e520cdc0dfd53290fd6525ae55f23793ba8b
Valeria829 <v.schelkanogova@gmail.com>
2023-06-29 09:46:08 +0500

    initial

---
+++ 4_1_class_work/4_1_class_work.py
@@ -0,0 +1,16 @@
+s = input()
+
+if s == s[::-1]:
+    print(True)
+else:
+    print(False)
+
+
+def str_pol():
+    s = input()
+    # print(True if s==s[::-1] else False)
+    print(s == s[::-1])
+
+str_pol()
+
+#
