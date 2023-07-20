24aad8410e15bea38b70e5e056d48d7bbdc524ad
Valeria829 <136175996+Valeria829@users.noreply.github.com>
2023-07-20 17:37:06 +0500

    Update views.py

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def index(response):
     return HttpResponse('Домашка по 4 занятию.')
 
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
